4fb4e5f381137881fd81d85f39dfb9929b834fae
wilemr de Mata <wilmerdemata149@gmail.com>
2025-07-01 09:56:26 -0600

    Creacion de datos de prueba

---
+++ productos.py
@@ -0,0 +1,5 @@
+producots = [
+    {"nombre": "laptop", "precio": 5990, "cantidad": 3},
+    {"nombre": "Monitor", "precio": 899, "cantidad": 6},
+    {"nombre": "Teclado", "precio": 199, "cantidad": 4},
+]
